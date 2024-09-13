19a2eb90addb8afbcf88b2d2dcde848b0b9760f8
skmayya <skandamayya4@gmail.com>
2024-09-13 17:56:07 +0530

    Remove .env file from repository and update .gitignore

--- auth.config.ts
+++
@@ -1,9 +0,0 @@
-import Google from "next-auth/providers/google"
-import type { NextAuthConfig } from "next-auth"
- 
-export default { providers: [
-    Google({
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET
-    })
-  ]} satisfies NextAuthConfig
